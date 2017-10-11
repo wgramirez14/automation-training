70efca8ebda84cc147d28545d6450b39d4d4a242
William Ramirez <william.ramirez@wnco.com>
2017-10-11 15:29:02 -0500

    Adding new methods to support training Task

---
+++ src/test/java/com/globant/training/automation_training_project/TestSuite.java
@@ -0,0 +1,125 @@
+package com.globant.training.automation_training_project;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestSuite
+{
+	@DataProvider(name = "numbersToMultiply")
+	public Object[][] numbersToBeMultiplied()
+	{
+		return new Object [][]{
+			{new Integer(5), new Integer(6)},
+			{new Integer(9),new Integer(15)},
+			{new Integer(150),new Integer(875)}
+		};
+	}
+	
+	@DataProvider(name = "stringValues")
+	public Object[][] stringValues ()
+	{
+		return new Object [][]{
+			{"Hola", "Mundo", "Feliz"},
+			{"Automation","Training","Test"},
+			{"We","are","ready"}
+		};
+	}
+		
+	@Test(dataProvider = "numbersToMultiply", groups = { "Smoke", "Regression" }, priority = 1)
+	public void multiplyNumbers(Integer n1, Integer n2)
+	{
+		System.out.println (n1 + " x " + n2 + " = " + n1*n2);
+	}
+	
+	@Test(dataProvider = "stringValues", groups = { "Smoke", "Regression" }, priority = 2)
+	public void concatenateStrings(String s1, String s2, String s3)
+	{
+		System.out.println (s1 + " " + s2 + " " + s3);
+	}
+	
+	@Parameters({"date"})
+	@Test(groups = {"Smoke"}, priority = 3)
+	public void printDate(String date)
+	{
+		System.out.println ("Date: " + date);
+	}
+	
+	@Parameters({"ambiente"})
+	@Test(groups = {"Smoke"}, priority = 4)
+	public void ambiente(String ambiente)
+	{
+		int n1 = 123, n2 = 987;
+		
+		if(ambiente.equals("AMBIENTE1")) {
+			
+			System.out.println ("Multiplicación: " + n1 + " x " + n2 + " = " + (n1*n2));
+		}
+		
+		else if(ambiente.equals("AMBIENTE2")) {
+			
+			System.out.println ("Resta: " + n1 + " - " + n2 + " = " + (n1-n2));
+		}
+		
+		else {
+			
+			System.out.println ("Parámetro no reconocido");
+		}		
+	}	
+	
+	@BeforeSuite(alwaysRun = true)
+	public void beforeSuite ()
+	{
+		System.out.println ("Before Suite");
+	}
+	
+	@BeforeGroups(alwaysRun = true)
+	public void beforeGroups ()
+	{
+		System.out.println ("Before Groups");
+	}
+	
+	@BeforeClass(alwaysRun = true)
+	public void beforeClass ()
+	{
+		System.out.println ("Before Class");
+	}
+	
+	@BeforeMethod(alwaysRun = true)
+	public void beforeMethod ()
+	{
+		System.out.println ("Before Method");
+	}
+	
+	@AfterMethod(alwaysRun = true)
+	public void afterMethod ()
+	{
+		System.out.println ("After Method");
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void afterClass ()
+	{
+		System.out.println ("After Class");
+	}
+	
+	@AfterSuite(alwaysRun = true)
+	public void afterSuite ()
+	{
+		System.out.println ("After Suite");
+	}	
+	
+	@AfterGroups(alwaysRun = true)
+	public void afterGroups ()
+	{
+		System.out.println ("After Groups");
+	}
+}
